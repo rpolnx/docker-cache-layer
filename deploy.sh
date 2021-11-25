170450ab3b0c1e2dd6d167ec825f955413e9ba37
Rodrigo Pereira <rodrigorpogo@gmail.com>
2021-11-24 22:31:40 -0300

    CI script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+export REPO=rpolnx
+export PROJECT_REPONAME=docker-nest-cache
+export LATEST_TAG=$(git rev-parse HEAD)
+export IMAGE_TAG=$(git rev-parse HEAD)
+
 export DOCKER_BUILDKIT=1 
 docker build --progress=plain \
                                 --cache-from $REPO/$PROJECT_REPONAME:builder \
@@ -14,5 +21,7 @@ docker build --progress=plain \
                          -t $REPO/$PROJECT_REPONAME:$IMAGE_TAG \
                          --build-arg BUILDKIT_INLINE_CACHE=1 .
 
-docker push $REPO/$PROJECT_REPONAME:builder
-docker push $REPO/$PROJECT_REPONAME:$LATEST_TAG
+docker push $REPO/$PROJECT_REPONAME --all-tags
+
+# docker push $REPO/$PROJECT_REPONAME:builder
+# docker push $REPO/$PROJECT_REPONAME:$LATEST_TAG
